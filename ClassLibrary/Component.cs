61625c1f69f56eaa6c1a6cee37f0df099594e7fe
Josh Meservey <joshua.meservey@smcltd.com>
2018-07-03 13:54:54 -0500

    Created ClassLibrary Classes.

--- ClassLibrary/Component.cs
+++ ClassLibrary/Component.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Drawing;
 using System.Text;
 using System.Threading.Tasks;
-using System.Drawing;
 using System.IO;
 
 namespace Toolroom_Scheduler
